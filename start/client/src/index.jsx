9446c568561dcccc2249e10a17a6dd129b9ad005
Jz1116 <vjiang1116@outlook.com>
2020-08-04 19:44:51 -0400

    Manage local state

--- start/client/src/index.jsx
+++ start/client/src/index.jsx
@@ -1,11 +1,25 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
-import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 import React from "react";
 import ReactDOM from "react-dom";
 import Pages from "./pages";
+import Login from "./pages/login";
 import injectStyles from "./styles";
+import { resolvers, typeDefs } from "./resolvers";
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -16,12 +30,21 @@ const link = new HttpLink({
 const client = new ApolloClient({
   cache,
   link,
+  typeDefs,
+  resolvers,
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem("token"),
+    cartItems: [],
+  },
 });
 
 injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById("root")
 );
